84279fd4de127cb0cd33587a3aed1c510a19eee1
arky-arky <61799378+arky-arky@users.noreply.github.com>
2023-03-12 18:03:46 +0200

    c++ addresses

---
+++ addresses.hpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+
+namespace addresses
+{
+    std::uintptr_t app = 0xFF8690;
+    std::uintptr_t client = 0xFF8AD0;
+    std::uintptr_t integrity_check = 0x3FBA99;
+    std::uintptr_t fps_limit = 0xF22DE0;
+    std::uintptr_t anti_bounce = 0x56C5B3;
+    std::uintptr_t is_collidable = 0x559260;
+    std::uintptr_t anzu = 0x21D041;
+    std::uintptr_t sendpacket = 0x5442C0;
+    std::uintptr_t sendpacketraw = 0x5446E0;
+    std::uintptr_t tankupdate = 0x3FCE90;
+    std::uintptr_t logtoconsole = 0x49E1B0;
+    std::uintptr_t trackpacket = 0x414A80;
+    std::uintptr_t textgame = 0x3FA3A0;
+}
