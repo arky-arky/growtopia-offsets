7ee7597f798de25beca67940ad1f8f4841568899
arky-arky <61799378+arky-arky@users.noreply.github.com>
2023-03-12 18:04:33 +0200

    c# offsets

---
+++ addresses.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace addresses
+{
+    public const Int32 app = 0xFF8690;
+    public const Int32 client = 0xFF8AD0;
+    public const Int32 integrity_check = 0x3FBA99;
+    public const Int32 fps_limit = 0xF22DE0;
+    public const Int32 anti_bounce = 0x56C5B3;
+    public const Int32 is_collidable = 0x559260;
+    public const Int32 anzu = 0x21D041;
+    public const Int32 sendpacket = 0x5442C0;
+    public const Int32 sendpacketraw = 0x5446E0;
+    public const Int32 tankupdate = 0x3FCE90;
+    public const Int32 logtoconsole = 0x49E1B0;
+    public const Int32 trackpacket = 0x414A80;
+    public const Int32 textgame = 0x3FA3A0;
+}
